9ba21648225fa0f90502abe26000ad02103a95b5
Bhargavghoniya <131822559+Bhargavghoniya@users.noreply.github.com>
2025-06-25 23:05:16 +0530

    Initial commit

---
+++ backend/routes/cart.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router();
+
+
+
+
+//route to get cart
+router.get('/userCart',)
+
+module.exports = router;
---
+++ backend/routes/user.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { sign } = require('jsonwebtoken');
+const { singup, login } = require('../controllers/user.controller');
+const router = express.Router();
+
+//singup route
+router.post('/register', singup);
+
+//login route
+router.post('/login',login);
+
+
+
+
+module.exports = router;
